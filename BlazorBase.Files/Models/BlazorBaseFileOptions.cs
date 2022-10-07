5e07faa241535d5a6ee7a8ed2f564d502c01938e
pal@acadon.de <pal@acadon.de>
2022-10-07 15:20:37 +0200

    added possibilty to ignore base file controller route

--- BlazorBase.Files/Models/BlazorBaseFileOptions.cs
+++ BlazorBase.Files/Models/BlazorBaseFileOptions.cs
@@ -21,6 +21,7 @@ public class BlazorBaseFileOptions : IBlazorBaseFileOptions
     public Type OptionsImportFromDatabaseEntryType { get; set; } = default!;
 
     public Type FileImplementationType { get; set; } = typeof(BaseFile);
+    public string ControllerRoute { get; set; } = "api/BaseFile";
     public string FileStorePath { get; set; } = @"C:\BlazorBaseFileStore";
     public string TempFileStorePath { get; set; } = @"C:\BlazorBaseFileStore\Temp";
     public bool AutomaticallyDeleteOldTemporaryFiles { get; set; } = true;
