43382abce4a65917a2c3e10db0213d92c47ac652
pal@acadon.de <pal@acadon.de>
2022-10-20 16:51:26 +0200

    fix bardropdown visibility bug on mobile phones

--- BlazorBase.CRUD/Components/BaseMenu.razor.cs
+++ BlazorBase.CRUD/Components/BaseMenu.razor.cs
@@ -1,4 +1,6 @@
-﻿using Blazorise.Icons.FontAwesome;
+﻿using BlazorBase.CRUD.Models;
+using Blazorise;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
 using System.Collections.Generic;
@@ -8,6 +10,8 @@ namespace BlazorBase.CRUD.Components
 {
     public partial class BaseMenu
     {
+        #region Parameters
+
         [Parameter]
         public object MenuName { get; set; }
 
@@ -15,14 +19,32 @@ namespace BlazorBase.CRUD.Components
         public object MenuIcon { get; set; }
 
         [Parameter]
-        public bool DropDownIsVisible { get; set; } = true;
+        public bool DropDownIsVisibleByDefault { get; set; } = true;
 
         [Parameter]
         public List<NavigationEntry> NavigationEntries { get; set; } = new List<NavigationEntry>();
 
-        public record NavigationEntry(string Name, string Link, string Icon);
-
         [Parameter(CaptureUnmatchedValues = true)]
         public IDictionary<string, object> AdditionalAttributes { get; set; }
+
+        #endregion
+
+        #region Member
+        protected bool DropDownIsVisible;
+        #endregion
+
+        #region Init
+
+        protected override void OnInitialized()
+        {
+            DropDownIsVisible = DropDownIsVisibleByDefault;
+        }
+
+        protected void DropDownVisibleChanged(bool visible)
+        {
+            DropDownIsVisible = visible; // Update parameter value, so by rerendering the bardropdown component the right visibility state will be handed over
+        }
+
+        #endregion
     }
 }
