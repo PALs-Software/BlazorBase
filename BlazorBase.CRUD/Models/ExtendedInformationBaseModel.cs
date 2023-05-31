9b3ba8b63108416fb80c8ffd26d8059b17f49b4b
pal@acadon.de <pal@acadon.de>
2023-05-31 17:19:11 +0200

    Added extended information base model with created by and last modified by flags

---
+++ BlazorBase.CRUD/Models/ExtendedInformationBaseModel.cs
@@ -0,0 +1,49 @@
+﻿using BlazorBase.CRUD.Attributes;
+using BlazorBase.CRUD.EventArguments;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace BlazorBase.CRUD.Models;
+
+public class ExtendedInformationBaseModel : BaseModel
+{
+    #region Properties
+
+    [Editable(false)]
+    [Visible(DisplayGroup = "Information", DisplayGroupOrder = 9999, DisplayOrder = 150, Collapsed = true)]
+    public virtual string CreatedBy { get; set; }
+
+    [Editable(false)]
+    [Visible(DisplayGroup = "Information", DisplayOrder = 250)]
+    public virtual string LastModifiedBy { get; set; }
+
+    #endregion
+
+    public override async Task OnBeforeDbContextAddEntry(OnBeforeDbContextAddEntryArgs args)
+    {
+        await base.OnBeforeDbContextAddEntry(args);
+        await FillModifiedByWithCurrentUserNameAsync(args.EventServices.ServiceProvider);
+    }
+
+    public override async Task OnBeforeDbContextModifyEntry(OnBeforeDbContextModifyEntryArgs args)
+    {
+        await base.OnBeforeDbContextModifyEntry(args);
+        await FillModifiedByWithCurrentUserNameAsync(args.EventServices.ServiceProvider);
+    }
+
+    protected async Task FillModifiedByWithCurrentUserNameAsync(IServiceProvider serviceProvider)
+    {
+        var authService = serviceProvider.GetService<AuthenticationStateProvider>();
+        if (authService == null)
+            return;
+
+        var authState = await authService.GetAuthenticationStateAsync();
+        if (String.IsNullOrEmpty(authState.User.Identity?.Name))
+            return;
+
+        LastModifiedBy = authState.User.Identity.Name;
+    }
+}
