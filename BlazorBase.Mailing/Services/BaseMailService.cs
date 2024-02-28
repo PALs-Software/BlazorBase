9dd4da3ca0ddbbcd7a4ba99429a9377db4b5b775
Patrick Londong <pal@acadon.de>
2024-02-28 12:36:15 +0100

    Add non async SendMail methods to BaseMailService

--- BlazorBase.Mailing/Services/BaseMailService.cs
+++ BlazorBase.Mailing/Services/BaseMailService.cs
@@ -12,18 +12,14 @@ using System.Runtime.Versioning;
 namespace BlazorBase.Mailing.Services;
 
 [SupportedOSPlatform("windows")]
-public class BaseMailService<TTemplateLocalizer> : BaseMailService
+public class BaseMailService<TTemplateLocalizer>(IBlazorBaseMailingOptions options, IServiceProvider serviceProvider, BaseErrorHandler errorHandler,
+                IStringLocalizer<BaseMailService> localizer,
+                IStringLocalizer<TTemplateLocalizer> templateLocalizer, ILogger<BaseMailService> logger) : BaseMailService(options, serviceProvider, errorHandler, localizer, logger)
 {
     #region Injects
-    protected readonly IStringLocalizer<TTemplateLocalizer> TemplateLocalizer;
-    #endregion
+    protected readonly IStringLocalizer<TTemplateLocalizer> TemplateLocalizer = templateLocalizer;
 
-    public BaseMailService(IBlazorBaseMailingOptions options, IServiceProvider serviceProvider, BaseErrorHandler errorHandler,
-                    IStringLocalizer<BaseMailService> localizer,
-                    IStringLocalizer<TTemplateLocalizer> templateLocalizer, ILogger<BaseMailService> logger) : base(options, serviceProvider, errorHandler, localizer, logger)
-    {
-        TemplateLocalizer = templateLocalizer;
-    }
+    #endregion
 
     /// <summary>
     /// Send an email asynchronously to the receivers with a specific template and arguments. Note that the first argument is always the website name as defined in the BlazorBaseOptions class <see cref="BlazorBase.Models.BlazorBaseOptions"/>
@@ -37,6 +33,20 @@ public class BaseMailService<TTemplateLocalizer> : BaseMailService
     public Task<bool> SendMailAsync(List<string> receivers, Enum mailTemplate, object[] subjectArguments, object[] bodyArguments, params string[] attachmentPathes)
     {
         return SendMailAsync(receivers, mailTemplate.ToString(), subjectArguments, bodyArguments, attachmentPathes);
+    }
+
+    /// <summary>
+    /// Send an email to the receivers with a specific template and arguments. Note that the first argument is always the website name as defined in the BlazorBaseOptions class <see cref="BlazorBase.Models.BlazorBaseOptions"/>
+    /// </summary>
+    /// <param name="receivers">The mail addresses of the receivers</param>
+    /// <param name="mailTemplate">The used mail template. The template localizer will look after the keys "{mailTemplate}_Subject" and "{mailTemplate}_Body"</param>
+    /// <param name="subjectArguments">The arguments for the localizer to replace in the subject template</param>
+    /// <param name="bodyArguments">The arguments for the localizter to replace in the body template</param>
+    /// <param name="attachmentPathes">File paths that are appended and sent with the email</param>
+    /// <returns>The task object representing the asynchronous operation</returns>
+    public bool SendMail(List<string> receivers, Enum mailTemplate, object[] subjectArguments, object[] bodyArguments, params string[] attachmentPathes)
+    {
+        return SendMail(receivers, mailTemplate.ToString(), subjectArguments, bodyArguments, attachmentPathes);
     }
 
     /// <summary>
@@ -54,6 +64,20 @@ public class BaseMailService<TTemplateLocalizer> : BaseMailService
     }
 
     /// <summary>
+    /// Send an email to the receiver with a specific template and arguments. Note that the first argument is always the website name as defined in the BlazorBaseOptions class <see cref="BlazorBase.Models.BlazorBaseOptions"/>
+    /// </summary>
+    /// <param name="receiver">The mail address of the receiver</param>
+    /// <param name="mailTemplate">The used mail template. The template localizer will look after the keys "{mailTemplate}_Subject" and "{mailTemplate}_Body"</param>
+    /// <param name="subjectArguments">The arguments for the localizer to replace in the subject template</param>
+    /// <param name="bodyArguments">The arguments for the localizter to replace in the body template</param>
+    /// <param name="attachmentPathes">File paths that are appended and sent with the email</param>
+    /// <returns>The task object representing the asynchronous operation</returns>
+    public bool SendMail(string receiver, Enum mailTemplate, object[] subjectArguments, object[] bodyArguments, params string[] attachmentPathes)
+    {
+        return SendMail(receiver, mailTemplate.ToString(), subjectArguments, bodyArguments, attachmentPathes);
+    }
+
+    /// <summary>
     /// Send an email asynchronously to the receiver with a specific template and arguments. Note that the first argument is always the website name as defined in the BlazorBaseOptions class <see cref="BlazorBase.Models.BlazorBaseOptions"/>
     /// </summary>
     /// <param name="receiver">The mail address of the receiver</param>
@@ -65,6 +89,20 @@ public class BaseMailService<TTemplateLocalizer> : BaseMailService
     public Task<bool> SendMailAsync(string receiver, string mailTemplate, object[] subjectArguments, object[] bodyArguments, params string[] attachmentPathes)
     {
         return SendMailAsync(new List<string>() { receiver }, mailTemplate.ToString(), subjectArguments, bodyArguments, attachmentPathes);
+    }
+
+    /// <summary>
+    /// Send an email to the receiver with a specific template and arguments. Note that the first argument is always the website name as defined in the BlazorBaseOptions class <see cref="BlazorBase.Models.BlazorBaseOptions"/>
+    /// </summary>
+    /// <param name="receiver">The mail address of the receiver</param>
+    /// <param name="mailTemplate">The used mail template. The template localizer will look after the keys "{mailTemplate}_Subject" and "{mailTemplate}_Body"</param>
+    /// <param name="subjectArguments">The arguments for the localizer to replace in the subject template</param>
+    /// <param name="bodyArguments">The arguments for the localizter to replace in the body template</param>
+    /// <param name="attachmentPathes">File paths that are appended and sent with the email</param>
+    /// <returns>The task object representing the asynchronous operation</returns>
+    public bool SendMail(string receiver, string mailTemplate, object[] subjectArguments, object[] bodyArguments, params string[] attachmentPathes)
+    {
+        return SendMail(new List<string>() { receiver }, mailTemplate.ToString(), subjectArguments, bodyArguments, attachmentPathes);
     }
 
     /// <summary>
@@ -87,18 +125,40 @@ public class BaseMailService<TTemplateLocalizer> : BaseMailService
         var body = TemplateLocalizer[$"{mailTemplate}_Body", bodyArguments].ToString();
         return SendMailAsync(receivers, subject, body, attachmentPathes);
     }
+
+    /// <summary>
+    /// Send an email to the receivers with a specific template and arguments. Note that the first argument is always the website name as defined in the BlazorBaseOptions class <see cref="BlazorBase.Models.BlazorBaseOptions"/>
+    /// </summary>
+    /// <param name="receivers">The mail addresses of the receivers</param>
+    /// <param name="mailTemplate">The used mail template. The template localizer will look after the keys "{mailTemplate}_Subject" and "{mailTemplate}_Body"</param>
+    /// <param name="subjectArguments">The arguments for the localizer to replace in the subject template</param>
+    /// <param name="bodyArguments">The arguments for the localizter to replace in the body template</param>
+    /// <param name="attachmentPathes">File paths that are appended and sent with the email</param>
+    /// <returns>The task object representing the asynchronous operation</returns>
+    public bool SendMail(List<string> receivers, string mailTemplate, object[] subjectArguments, object[] bodyArguments, params string[] attachmentPathes)
+    {
+        var defaultSubjectArguments = new object[] { MailingOptions.WebsiteName };
+        var defaultBodyArguments = new object[] { MailingOptions.WebsiteName };
+        subjectArguments = defaultSubjectArguments.Concat(subjectArguments).ToArray();
+        bodyArguments = defaultBodyArguments.Concat(bodyArguments).ToArray();
+
+        var subject = TemplateLocalizer[$"{mailTemplate}_Subject", subjectArguments].ToString();
+        var body = TemplateLocalizer[$"{mailTemplate}_Body", bodyArguments].ToString();
+        return SendMail(receivers, subject, body, attachmentPathes);
+    }
 }
 
 [SupportedOSPlatform("windows")]
-public class BaseMailService
+public class BaseMailService(IBlazorBaseMailingOptions options, IServiceProvider serviceProvider, BaseErrorHandler errorHandler,
+                   IStringLocalizer<BaseMailService> localizer, ILogger<BaseMailService> logger)
 {
     #region Injects
 
-    protected readonly IBlazorBaseMailingOptions MailingOptions;
-    protected readonly IServiceProvider ServiceProvider;
-    protected readonly BaseErrorHandler ErrorHandler;
-    protected readonly IStringLocalizer<BaseMailService> Localizer;
-    protected readonly ILogger<BaseMailService> Logger;
+    protected readonly IBlazorBaseMailingOptions MailingOptions = options;
+    protected readonly IServiceProvider ServiceProvider = serviceProvider;
+    protected readonly BaseErrorHandler ErrorHandler = errorHandler;
+    protected readonly IStringLocalizer<BaseMailService> Localizer = localizer;
+    protected readonly ILogger<BaseMailService> Logger = logger;
 
     #endregion
 
@@ -109,16 +169,6 @@ public class BaseMailService
     public string? LastErrorMessage { get; protected set; } = null;
 
     #endregion
-
-    public BaseMailService(IBlazorBaseMailingOptions options, IServiceProvider serviceProvider, BaseErrorHandler errorHandler,
-                       IStringLocalizer<BaseMailService> localizer, ILogger<BaseMailService> logger)
-    {
-        MailingOptions = options;
-        ServiceProvider = serviceProvider;
-        ErrorHandler = errorHandler;
-        Localizer = localizer;
-        Logger = logger;
-    }
 
     /// <summary>
     /// Send an email asynchronously to the receiver
@@ -131,6 +181,19 @@ public class BaseMailService
     public Task<bool> SendMailAsync(string receiver, string subject, string body, params string[] attachmentPathes)
     {
         return SendMailAsync(new List<string>() { receiver }, subject, body, attachmentPathes);
+    }
+
+    /// <summary>
+    /// Send an email to the receiver
+    /// </summary>
+    /// <param name="receiver">The mail address of the receiver</param>
+    /// <param name="subject">The subject text of the mail</param>
+    /// <param name="body">The body text of the mail</param>
+    /// <param name="attachmentPathes">File paths that are appended and sent with the email</param>
+    /// <returns>The task object representing the asynchronous operation</returns>
+    public bool SendMail(string receiver, string subject, string body, params string[] attachmentPathes)
+    {
+        return SendMail(new List<string>() { receiver }, subject, body, attachmentPathes);
     }
 
     /// <summary>
@@ -147,50 +210,89 @@ public class BaseMailService
 
         try
         {
-            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
-
-            var client = new SmtpClient()
-            {
-                Host = MailingOptions.Server,
-                UseDefaultCredentials = MailingOptions.UseDefaultCredentials,
-                Port = MailingOptions.Port,
-                EnableSsl = MailingOptions.EnableSSL,
-            };
-            if (!MailingOptions.UseDefaultCredentials)
-                client.Credentials = new NetworkCredential(MailingOptions.Host, MailingOptions.HostPassword, MailingOptions.Domain);
-
-            var mailMessage = new MailMessage
-            {
-                From = new MailAddress(MailingOptions.SenderAddress),
-                Body = body,
-                Subject = subject,
-                IsBodyHtml = true
-            };
-
-            foreach (var item in attachmentPathes)
-                mailMessage.Attachments.Add(new Attachment(item));
-
-            foreach (var item in receivers)
-                if (!String.IsNullOrEmpty(item))
-                    mailMessage.To.Add(item);
-
-            await client.SendMailAsync(mailMessage);
+            var preparedMail = PrepareMail(receivers, subject, body, attachmentPathes);
+            await preparedMail.Client.SendMailAsync(preparedMail.MailMessage);
         }
         catch (Exception e)
         {
-            var errorMessage = ErrorHandler.PrepareExceptionErrorMessage(e);
-            LastErrorMessage = errorMessage;
-            Logger.LogError("Error sending email to {Receivers} with subject {Subject}:\n\n{ErrorMessage}", String.Join(";", receivers), subject, errorMessage);
-
-            if (DisplayErrorMessagesToUser)
-            {
-                var messageHandler = ServiceProvider.GetRequiredService<IMessageHandler>();
-                messageHandler.ShowMessage(Localizer["Error sending email to {0} with subject {1}", String.Join(";", receivers), subject], errorMessage, MessageType.Error);
-            }
-
+            HandleMailError(receivers, subject, e);
             return false;
         }
 
         return true;
     }
+
+    /// <summary>
+    /// Send an email to the receivers
+    /// </summary>
+    /// <param name="receivers">The mail addresses of the receivers</param>
+    /// <param name="subject">The subject text of the mail</param>
+    /// <param name="body">The body text of the mail</param>
+    /// <param name="attachmentPathes">File paths that are appended and sent with the email</param>
+    /// <returns>The task object representing the asynchronous operation</returns>
+    public bool SendMail(List<string> receivers, string subject, string body, params string[] attachmentPathes)
+    {
+        LastErrorMessage = null;
+
+        try
+        {
+            var preparedMail = PrepareMail(receivers, subject, body, attachmentPathes);
+            preparedMail.Client.Send(preparedMail.MailMessage);
+        }
+        catch (Exception e)
+        {
+            HandleMailError(receivers, subject, e);
+            return false;
+        }
+
+        return true;
+    }
+
+    protected virtual (SmtpClient Client, MailMessage MailMessage) PrepareMail(List<string> receivers, string subject, string body, params string[] attachmentPathes)
+    {
+#if DEBUG
+        ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+#endif
+
+        var client = new SmtpClient()
+        {
+            Host = MailingOptions.Server,
+            UseDefaultCredentials = MailingOptions.UseDefaultCredentials,
+            Port = MailingOptions.Port,
+            EnableSsl = MailingOptions.EnableSSL,
+        };
+        if (!MailingOptions.UseDefaultCredentials)
+            client.Credentials = new NetworkCredential(MailingOptions.Host, MailingOptions.HostPassword, MailingOptions.Domain);
+
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress(MailingOptions.SenderAddress),
+            Body = body,
+            Subject = subject,
+            IsBodyHtml = true
+        };
+
+        foreach (var item in attachmentPathes)
+            mailMessage.Attachments.Add(new Attachment(item));
+
+        foreach (var item in receivers)
+            if (!String.IsNullOrEmpty(item))
+                mailMessage.To.Add(item);
+
+        return (client, mailMessage);
+    }
+
+    protected virtual void HandleMailError(List<string> receivers, string subject, Exception e)
+    {
+        var errorMessage = ErrorHandler.PrepareExceptionErrorMessage(e);
+        LastErrorMessage = errorMessage;
+        Logger.LogError("Error sending email to {Receivers} with subject {Subject}:\n\n{ErrorMessage}", String.Join(";", receivers), subject, errorMessage);
+
+        if (DisplayErrorMessagesToUser)
+        {
+            var messageHandler = ServiceProvider.GetRequiredService<IMessageHandler>();
+            messageHandler.ShowMessage(Localizer["Error sending email to {0} with subject {1}", String.Join(";", receivers), subject], errorMessage, MessageType.Error);
+        }
+    }
+
 }
