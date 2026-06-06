223ecfa6e491de0e65d6757f8162409dd21cdc65
Patrick Londong <pal@pals-software.de>
2026-06-07 00:32:32 +0200

    Add possibility to group menu sections

--- BlazorBase.CRUD/Models/NavigationEntry.cs
+++ BlazorBase.CRUD/Models/NavigationEntry.cs
@@ -2,14 +2,16 @@
 
 public class NavigationEntry
 {
-    public NavigationEntry(string name, string link, string icon)
+    public NavigationEntry(string name, string link, string icon, string? group = null)
     {
         Name = name;
         Link = link;
         Icon = icon;
+        Group = group;
     }
 
     public string? Name { get; set; }
     public string? Link { get; set; }
     public string? Icon { get; set; }
+    public string? Group { get; set; }
 }
