98dac475edc76ccef23c12556910a2efee63557d
Patrick Londong <pal@pals-software.de>
2026-06-07 11:33:30 +0200

    Add open in new tab functionality to BaseMenu

--- BlazorBase.CRUD/Models/NavigationEntry.cs
+++ BlazorBase.CRUD/Models/NavigationEntry.cs
@@ -2,16 +2,18 @@
 
 public class NavigationEntry
 {
-    public NavigationEntry(string name, string link, string icon, string? group = null)
+    public NavigationEntry(string name, string link, string icon, string? group = null, bool openInNewTab = false)
     {
         Name = name;
         Link = link;
         Icon = icon;
         Group = group;
+        OpenInNewTab = openInNewTab;
     }
 
     public string? Name { get; set; }
     public string? Link { get; set; }
     public string? Icon { get; set; }
     public string? Group { get; set; }
+    public bool OpenInNewTab { get; set; }
 }
